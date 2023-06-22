739e839e8771583a70201893d2aac80a780f9c2a
denisb-extra <45913485+denisb-extra@users.noreply.github.com>
2023-06-22 10:50:26 +0300

    big update

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,15 @@
 $(document).ready(function ($) {
-
+    window.addEventListener('scroll', onScroll);
+    function onScroll(e){
+        var distanceY = window.pageYOffset || document.documentElement.scrollTop;
+            shrinkOn = 30;
+        if (distanceY > shrinkOn) {
+            $("header" ).addClass("scrolled");
+        } else {
+            $("header" ).removeClass("scrolled");
+        }
+    }
+    onScroll();
     $(".mobile_menu").simpleMobileMenu({
         "menuStyle": "slide",
     });
